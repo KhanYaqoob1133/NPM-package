755c3faf3f85f5e6b13f110b881cbfc69292d3b8
KhanYaqoob1133 <fa21-bcs-066@cuilahore.edu.pk>
2024-11-24 18:06:53 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+export function greet(name = 'Guest') {
+    return `Hello, ${name}! Welcome to my package.`;
+}
+
+export function farewell(name = 'Guest') {
+    return `Goodbye, ${name}! See you next time.`;
+}
